724f8dfa64a57d1c42538c870b94a301046dd7ac
hinst <hinstance@yandex.ru>
2018-04-18 23:53:35 +0300

    Implemented basic navigation [<] left [>] right

--- web/src/Gallery.tsx
+++ web/src/Gallery.tsx
@@ -3,7 +3,8 @@
 namespace hImageStorage {
     export class Gallery extends Widget {
         imageUI: HTMLElement;
-        list: string[];
+        list: string[] = [];
+        currentImageIndex = 0;
 
         _fitHeight: boolean;
         set fitHeight(fit: boolean) {
@@ -12,8 +13,10 @@ namespace hImageStorage {
         }
 
         init() {
-            this.menuBar.append(<button class="w3-btn w3-black">&lt;</button>);
-            this.menuBar.append(<button class="w3-btn w3-black">&gt;</button>);
+            this.menuBar.append(<button class="w3-btn w3-black" name="ltButton">&lt;</button>);
+            this.menuBar.on("click", "[name='ltButton']", () => this.receiveNavigateClick(false));
+            this.menuBar.append(<button class="w3-btn w3-black" name="rtButton">&gt;</button>);
+            this.menuBar.on("click", "[name='rtButton']", () => this.receiveNavigateClick(true));
             this.element.append(
                 <div>
                     <hr style="margin-top: 0px; margin-bottom: 0px;"/>
@@ -51,6 +54,21 @@ namespace hImageStorage {
 
         updateImageHeight() {
             $(this.imageUI).css("max-height", this._fitHeight ? window.innerHeight : "");
+        }
+
+        receiveNavigateClick(right: boolean): any {
+            if (this.list.length > 0) {
+                if (right) {
+                    this.currentImageIndex++;
+                    if (this.currentImageIndex >= this.list.length)
+                        this.currentImageIndex = this.list.length;
+                } else {
+                    this.currentImageIndex--;
+                    if (this.currentImageIndex <= 0)
+                        this.currentImageIndex = 0;
+                }
+                this.loadImage(this.list[this.currentImageIndex]);
+            }
         }
     }
 }
--- web/src/Widget.tsx
+++ web/src/Widget.tsx
@@ -1,12 +1,18 @@
 /// <reference path="WebPath.ts"/>
 
 namespace hImageStorage {
+    var UID = 0;
+    const ElementIdPrefix = "hImageStorage_element_";
     export class Widget {
         element: JQuery = $(<div></div>);
         webPath: hts.WebPath;
         appPath: string;
         menuBar: JQuery;
         init() {
+        }
+        static getUID() {
+            ++UID;
+            return  ElementIdPrefix + UID;
         }
     }
 }
